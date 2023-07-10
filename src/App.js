41098e7af55923d8de19d6edcf1926329aee403a
Jn-Sajol <julkar10121@gmail.com>
2023-07-10 18:53:51 +0600

    check calculator

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import { useMemo, useState } from "react";
+import "./index.css";
 
-function App() {
+const DEFAULT_WEIGHT = 50;
+const DEFAULT_HEIGHT = 150;
+
+export default function App() {
+  const [height, setHeight] = useState(DEFAULT_HEIGHT);
+  const [weight, setWeight] = useState(DEFAULT_WEIGHT);
+
+  function onHeightChange(event) {
+    const inputHeight = event.target.value;
+    setHeight(inputHeight);
+  }
+
+  function onWeightChange(event) {
+    const inputWeight = event.target.value;
+    setWeight(inputWeight);
+  }
+
+  const output = useMemo(() => {
+    const calculatedHeight = height / 100;
+    return (weight / (calculatedHeight * calculatedHeight)).toFixed(1);
+  }, [weight, height]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="container">
+      <h1 className="heder">BMI CALCULATOR</h1>
+      <div className="input-section">
+        <p class="slider-output">Weight: {weight} kg</p>
+        <input
+          className="input-slider"
+          onChange={onWeightChange}
+          type="range"
+          step="1"
+          min="40"
+          max="220"
+        />
+        <p class="slider-output">Height: {height} cm</p>
+        <input
+          className="input-slider"
+          onChange={onHeightChange}
+          type="range"
+          min="140"
+          max="220"
+        />
+      </div>
+      <div className="output-section">
+        <p>Your BMI is</p>
+        <p className="output">{output}</p>
+      </div>
+    </main>
   );
 }
-
-export default App;
